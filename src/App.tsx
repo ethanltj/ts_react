a9971e14f59424b129e6cccf688ee213e524214f
bob lee <boblee@13LIs-MacBook-Pro.local>
2020-09-13 19:53:41 -0400

    add like button

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Hello from './components/Hello'
+import LikeButoon from './components/Likebutton'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <Hello message = "blotbj Hello"/>
+        <LikeButoon />
         <a
           className="App-link"
           href="https://reactjs.org"
